ce4969047ebe6114af69baa2ced049713d14f10b
Cleber Fadil <cleber.fadil@gmail.com>
2024-10-08 14:13:15 -0300

    delete method implementhed

--- src/main/java/dev/galileu/aulamongo/services/UserService.java
+++ src/main/java/dev/galileu/aulamongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.galileu.aulamongo.domain.User;
+import dev.galileu.aulamongo.dto.UserDTO;
 import dev.galileu.aulamongo.repositories.UserRepository;
 import dev.galileu.aulamongo.services.exception.ObjectNotFoundException;
 
@@ -18,13 +19,25 @@ public class UserService {
 
     }
 
-    public User findById(String id) {
+    public User findById(String id) { 
         User user = repo.findById(id).orElse(null);
         if (user == null) {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
         return repo.findById(id).get();
+    }
 
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
     }
 
 }
