ea49ba4b3fe92c4f40f46b8488fa27dc71051282
Cleber Fadil <cleber.fadil@gmail.com>
2024-10-09 11:45:14 -0300

    Consulta Simples com query methods

--- src/main/java/dev/galileu/aulamongo/services/PostService.java
+++ src/main/java/dev/galileu/aulamongo/services/PostService.java
@@ -1,5 +1,7 @@
 package dev.galileu.aulamongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,10 @@ public class PostService {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
         return repo.findById(id).get();
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
