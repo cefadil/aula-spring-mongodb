50dec0eeaa52b3da29006487054ea9d1cfd2966a
Cleber Fadil <cleber.fadil@gmail.com>
2024-10-09 11:53:55 -0300

    Consulta simples com @Query

--- src/main/java/dev/galileu/aulamongo/repositories/PostRepository.java
+++ src/main/java/dev/galileu/aulamongo/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package dev.galileu.aulamongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.galileu.aulamongo.domain.Post;
     
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
